fdc376d3543956063fcf7ad12c03ee4f1fdc5f29
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-05-12 18:29:29 -0400

    feat: HU-01 registro de usuario

---
+++ backend/src/main/java/com/ucb/farmago/backend/services/UsuarioService.java
@@ -0,0 +1,25 @@
+package com.ucb.farmago.backend.services;
+
+import com.ucb.farmago.backend.models.Usuario;
+import com.ucb.farmago.backend.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public Usuario registrar(Usuario usuario) {
+        if (usuarioRepository.existsByEmail(usuario.getEmail())) {
+            throw new RuntimeException("El email ya está registrado");
+        }
+        usuario.setPasswordHash(passwordEncoder.encode(usuario.getPasswordHash()));
+        usuario.setRol("CLIENTE");
+        return usuarioRepository.save(usuario);
+    }
+}
