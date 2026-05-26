caaf960a84558c1826bdeb45f8742408bcabdc99
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-05-26 00:36:13 -0400

    revert: quitar Navbar del PR de backend

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -49,7 +49,7 @@ export default function Navbar() {
             </li>
 
             <li>
-              <Link href="/contactos" className="nav-link">
+              <Link href="contactos" className="nav-link">
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.35 2 2 0 0 1 3.6 1h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L7.91 8.6a16 16 0 0 0 6 6l.92-.92a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16" />
                 </svg>
